1ff940d84b03064c1d5f85aad0a1cc336517602b
DavidCorreas <davidestu77@gmail.com>
2023-06-27 14:21:46 +0000

    Update: CORS and Dockerfile

--- src/app.py
+++ src/app.py
@@ -9,6 +9,13 @@ from interpreter import InterpreterManager
 app = FastAPI()
 interpreters = InterpreterManager()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 class Command(BaseModel):
     """
